bae0acf75a7dac8d0fc19ffaa45a672cca8f74e1
xtra_noobies <fatimazakiyah10@gmail.com>
2026-09-14 22:11:54 +0700

    Menambahkan boolean.h

---
+++ boolean.h
@@ -0,0 +1,13 @@
+#ifndef boolean_H
+#define boolean_H 
+
+/* Program   : boolean.h */
+/* Deskripsi : boolean */
+/* Pembuat   : fmmkyh (on Github) */
+/***********************************/
+
+#define true 1
+#define false 0
+#define boolean unsigned char
+
+#endif
